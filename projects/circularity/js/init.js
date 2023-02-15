f0b9da852f0d297738df3373c8ebef51a3460b73
Ethan <cadetenaquin@nomma.net>
2023-02-15 17:00:06 +0000

    day 5

--- projects/circularity/js/init.js
+++ projects/circularity/js/init.js
@@ -69,20 +69,18 @@ var init = function (window) {
         game.checkCirclePosition = function(circle) {
 
             // if the circle has gone past the RIGHT side of the screen then place it on the LEFT
-            if ( circle.x > canvas.width ) {
+            if (circle.x > canvas.width) {
                 circle.x = 0;
             }    
             // TODO 6 : YOUR CODE STARTS HERE //////////////////////
-            if ( circle.x < 0 ) {
+            if (circle.x < 0) {
                 circle.x = canvas.width;
             }
-             if ( circle.y < canvas.length ) {
+             if (circle.y > canvas.height) {
                 circle.y = 0;
-                
-            if ( circle.y > canvas.length ) {
-                circle.y = 0;
-            }
-           
+             }
+            if (circle.y < 0) {
+                circle.y = canvas.height;
             }
 
             // YOUR TODO 6 CODE ENDS HERE //////////////////////////
