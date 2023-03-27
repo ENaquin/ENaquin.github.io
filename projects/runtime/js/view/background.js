832795d4869b5b25bad31c85454938e76b8c3f01
Ethan <cadetenaquin@nomma.net>
2023-03-27 19:58:06 +0000

    bruh

--- projects/runtime/js/view/background.js
+++ projects/runtime/js/view/background.js
@@ -1,33 +1,33 @@
 var background = function (window) {
     'use strict';
-    
+
     window.opspark = window.opspark || {};
     var draw = window.opspark.draw;
     var createjs = window.createjs;
-    
+
     /*
      * Create a background view for our game application
      */
-    window.opspark.makeBackground = function(app,ground) {
+    window.opspark.makeBackground = function (app, ground) {
         /* Error Checking - DO NOT DELETE */
-        if(!app) {
+        if (!app) {
             throw new Error("Invalid app argument");
         }
-        if(!ground || typeof(ground.y) == 'undefined') {
+        if (!ground || typeof (ground.y) == 'undefined') {
             throw new Error("Invalid ground argument");
         }
-        
+
         // useful variables
         var canvasWidth = app.canvas.width;
         var canvasHeight = app.canvas.height;
         var groundY = ground.y;
-        
+
         // container which will be returned
         var background;
-        
+
         // ANIMATION VARIABLES HERE:
-        
-     
+        var tree
+
         // called at the start of game and whenever the page is resized
         // add objects for display in background. draws each image added to the background once
         function render() {
@@ -36,21 +36,36 @@ var background = function (window) {
             // TODO: 2 - Part 2
             // this fills the background with a obnoxious yellow
             // you should modify this to suit your game
-            var backgroundFill = draw.rect(canvasWidth,canvasHeight,'yellow');
+            var backgroundFill = draw.rect(canvasWidth, canvasHeight = groundY, 'red');
             background.addChild(backgroundFill);
-            
             // TODO: 3 - Add a moon and starfield
-            
-            
+            var moon = draw.bitmap("https://o.remove.bg/downloads/b53518e8-3134-42f8-ac3b-862f5c8b4d31/korn-removebg-preview.png");
+            moon.x = 1000;
+            moon.y = -100;
+            moon.scaleX = 0.95;
+            moon.scaleY = 0.95;
+            background.addChild(moon);
+
+            for (var i = 0; i < 150; i++) {
+                var circle = draw.circle(10, "white", "LightGray", 2);
+                circle.x = canvasWidth * Math.random();
+                circle.y = groundY * Math.random();
+                background.addChild(circle);
+            }
+
             // TODO 5: Part 1 - Add buildings!     Q: This is before TODO 4 for a reason! Why?
-            
-            
+
+
             // TODO 4: Part 1 - Add a tree
-            
-            
+            tree = draw.bitmap("https://o.remove.bg/downloads/48313df3-1788-4d93-bd22-d2fb6e946275/jonathan_davis-removebg-preview__1_-removebg-preview.png");
+            tree.x = 100;
+            tree.y = 100;
+            background.addChild(tree);
+
+
         } // end of render function - DO NOT DELETE
-        
-        
+
+
         // Perform background animation
         // called on each timer "tick" - 60 times per second
         function update() {
@@ -58,26 +73,29 @@ var background = function (window) {
             var canvasWidth = app.canvas.width;
             var canvasHeight = app.canvas.height;
             var groundY = ground.y;
-            
+
             // TODO 4: Part 2 - Move the tree!
-            
-            
+            tree.x = tree.x + 1;
+
+            if (tree.x < -200) {
+                tree.x = canvasWidth;
+            }
             // TODO 5: Part 2 - Parallax
-            
+
 
         } // end of update function - DO NOT DELETE
-        
-        
-        
+
+
+
         /* Make a createjs Container for the background and let it know about the render and upate functions*/
         background = new createjs.Container();
         background.resize = render;
         background.update = update;
-        
+
         /* make the background able to respond to resizing and timer updates*/
         app.addResizeable(background);
         app.addUpdateable(background);
-        
+
         /* render and return the background */
         render();
         return background;
@@ -85,7 +103,7 @@ var background = function (window) {
 };
 
 // DON'T REMOVE THIS CODE //////////////////////////////////////////////////////
-if((typeof process !== 'undefined') &&
+if ((typeof process !== 'undefined') &&
     (typeof process.versions.node !== 'undefined')) {
     // here, export any references you need for tests //
     module.exports = background;
